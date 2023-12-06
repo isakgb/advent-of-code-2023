067b996b04c5fa5047cdef5249a19ffe43431691
isakgb <33453353+isakgb@users.noreply.github.com>
2023-12-06 20:10:52 +0100

    Add working day 5 part 2

--- days/05_02v2.py
+++ days/05_02v2.py
@@ -3,13 +3,7 @@ from typing import List
 
 
 groups = get_input(5).strip()
-
-
 groups = apply(groups, lambda x: x.split("\n\n"))
-#groups = apply(groups, lambda x: x.split(": ")[1].split(" | "))
-#groups = apply(groups, ints)
-
-print(groups)
 
 seeds, rules = groups[0], groups[1:]
 
@@ -18,12 +12,11 @@ rules = apply(rules, lambda x: x.split(":")[1].split("\n")[1:])
 seeds = seeds.split(": ")[1].split()
 seeds = apply(seeds, int)
 
-seeds = [0, 1000000000000]
-
 
 def sort_rule(rule):
     sorted_rule = sorted(rule, key=lambda x: int(x.split()[1]))
     return [[int(y) for y in x.split()] for x in sorted_rule]
+
 
 rules = [sort_rule(rule) for rule in rules]
 
@@ -47,23 +40,26 @@ class Range:
         result = RangeList([])
 
         if self.start + self.length <= rule.src:
-            result.ranges.append(Range(self.start, self.length))
+            # Range is before rule. Don't do anything
             return result
         if self.start >= rule.src + rule.length:
-            result.ranges.append(Range(self.start, self.length))
+            # Range is after rule. Don't do anything
             return result
         if self.start < rule.src:
+            # Range starts before rule
             preoverlap = rule.src - self.start
             result.ranges.append(Range(self.start, preoverlap))
-            overlap = min(self.length - preoverlap, rule.length)
-            result.ranges.append(Range(rule.dst, self.start + preoverlap + overlap - rule.src))
-            self.length -= preoverlap + overlap
-            self.start += preoverlap + overlap
-            if self.length > 0:
-                print("self.length > 0", self.start, self.length, rule.src + rule.length)
+            self.length -= preoverlap
+            self.start += preoverlap
+        if self.start >= rule.src:
+            # (Remaining) Range starts at or after the rule
+            endpoint = min(rule.src + rule.length, self.start + self.length)
+            overlap = endpoint - self.start
 
-            return result
-        result.ranges.append(Range(self.start, self.length))
+            result.ranges.append(Range(rule.dst + self.start - rule.src, overlap))
+            self.length -= overlap
+            self.start += overlap
+
         return result
 
     def __repr__(self):
@@ -75,7 +71,15 @@ class RangeList:
         self.ranges = ranges
 
     def add_ruleset(self, rules: List[Rule]):
-        pass
+        result = RangeList([])
+        for range in self.ranges:
+            for rule in rules:
+                result.ranges.extend(range.apply_rule(rule).ranges)
+                if range.length == 0:
+                    break
+            if range.length > 0:
+                result.ranges.append(range)
+        return result
 
     def __repr__(self):
         return f"RangeList({self.ranges})"
@@ -84,9 +88,7 @@ class RangeList:
         return sum(r.length for r in self.ranges)
 
 
-print(rules)
 rules = [[Rule(dst, src, length) for dst, src, length in mappings] for mappings in rules]
-print(rules)
 
 seed_parsed = []
 
@@ -96,71 +98,10 @@ for i in range(len(seeds)//2):
     length = seeds[si+1]
     seed_parsed.append(Range(start, length))
 
-print(seed_parsed)
-print(seed_parsed[0].apply_rule(rules[0][0]))
-def location_number_range(n):
 
-    for rule in rules:
-        #print(n)
-        new_n = []
-        for asd in n:
-            n_start, n_length = asd
-            for dst, src, rule_length in rule:
-                if n_length == 0:
-                    break
-                if src == 0 and n_start == 0:
-                    print("Both are 0!")
+rangelist = RangeList(seed_parsed)
 
-                if src <= n_start < src + rule_length:
-                    overlap = min(src + rule_length, n_start + n_length) - n_start
+for ruleset in rules:
+    rangelist = rangelist.add_ruleset(ruleset)
 
-                    new_n.append([dst + (n_start - src), overlap])
-                    #print("Found full overlap with rule", dst, src, rule_length, "our seeds",n_start, n_length, overlap)
-                    n_start += overlap
-                    n_length -= overlap
-                    continue
-
-                elif n_start < src <= n_start + n_length:
-                    #print("Found partial overlap with rule", dst, src, rule_length, "our seeds",n_start, n_length)
-                    nonoverlap = src - n_start
-                    #print(" - Nonoverlap", n_start, nonoverlap)
-                    new_n.append([n_start, nonoverlap])
-                    n_start += nonoverlap
-                    n_length -= nonoverlap
-                    overlap = min(rule_length, n_length)
-                    new_n.append([dst, overlap])
-
-                    n_start += overlap
-                    n_length -= overlap
-                    #print(" - Overlap", dst, overlap)
-                    #print(" - New n", n_start, n_length)
-                    #if dst == 0:
-                        #print("xddd", dst, src, rule_length, n_start, n_length, overlap)
-                        #print(rule)
-            if n_length > 0:
-                #print("No overlap with rule", n_start, n_length)
-                new_n.append([n_start, n_length])
-
-        n = new_n
-        print("Total seeds", sum([x[1] for x in n]), len(n))
-    print("Done")
-    for i in n:
-        print(i)
-    #print(n)
-    return min([x[0] for x in n])
-
-
-lowest = 1e100
-
-print(seeds)
-
-seed_parsed = []
-
-
-
-lowest = location_number_range(seed_parsed)
-
-
-print(lowest)
-
-# 55413352 is too high
+print(min(r.start for r in rangelist.ranges))
